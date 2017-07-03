30c9d78124989ec12b9bdf42b04b3981034bbc65
mnirkko <m.nirkko@sussex.ac.uk>
2017-07-03 23:52:33 +0100

    Minor fix.

--- pca_runs/check_files.sh
+++ pca_runs/check_files.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "Files for the following runs do not exist:"
+export FPATH="/lustre/scratch/epp/neutrino/snoplus/TELLIE_PCA_RUNS_PROCESSED"
 while read RUN; do
-  if [ ! -f "/lustre/scratch/epp/neutrino/snoplus/TELLIE_PCA_RUNS_PROCESSED/Analysis_r0000${RUN}_s000_p000.root" ]; then
+  if [ ! -f "${FPATH}/Analysis_r0000${RUN}_s000_p000.root" ] && [ ! -f "${FPATH}/Analysis_r0000${RUN}_s000_p001.root" ] && [ ! -f "${FPATH}/Analysis_r0000${RUN}_s000_p002.root" ] && [ ! -f "${FPATH}/Analysis_r0000${RUN}_s000_p003.root" ]; then
     echo $RUN
   fi
 done <$1
