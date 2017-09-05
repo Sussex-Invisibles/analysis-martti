2c31fa75edcec558a11e889eee5a835415565035
mnirkko <m.nirkko@sussex.ac.uk>
2017-09-05 12:58:29 +0100

    Last changes to PIN readings.

--- pin_readings/pin_readings.C
+++ pin_readings/pin_readings.C
@@ -54,7 +54,7 @@ using namespace std;
 int main(int argc, char** argv) {
   
   // Test flag (0 = process all runs, else run number)
-  const int TEST = (RUN_CLUSTER) ? 0 : 100801;
+  const int TEST = (RUN_CLUSTER) ? 0 : 1;
 
   // Loop over all fibres in list
   string input1 = "TELLIE_PCA_set1.txt";
@@ -62,8 +62,8 @@ int main(int argc, char** argv) {
   if (!in1) { cerr<<"Failed to open TELLIE_PCA_set1.txt"<<endl; exit(1); }
   //TH2F *nhits = new TH2F("nhits","NHit/event vs PIN reading",100,0,1e4,100,0,100);
   string line;
-  float subrunpin[40], subrunrms[40];
-  float avgnhit[40];
+  float subrunpin[40]={0}, subrunrms[40]={0};
+  float avgnhit[40]={0};
   int run, subrun, pin;
   float rms;
   int nsubruns=0;
@@ -74,7 +74,7 @@ int main(int argc, char** argv) {
   while (true) {
     in1 >> run >> subrun >> pin >> rms;
     if (!in1.good()) break;
-    if (TEST && TEST!=run) continue; // only want specified run
+    if (TEST && lastrun!=0 && run!=lastrun) continue; // only want one run
     subrunpin[subrun] = pin;
     subrunrms[subrun] = rms;
     if (run == lastrun) continue; // only do this once per run
@@ -94,21 +94,64 @@ int main(int argc, char** argv) {
     cerr<<"*** ERROR *** No input files found."<<endl;
     return 1; 
   }
+  TGraph *g = new TGraph(nsubruns,subrunpin,avgnhit);
+
+  string input2 = "TELLIE_PCA_set2.txt";
+  ifstream in2(input2.c_str());
+  if (!in2) { cerr<<"Failed to open TELLIE_PCA_set2.txt"<<endl; exit(1); }
+  float subrunpin2[40]={0}, subrunrms2[40]={0};
+  float avgnhit2[40]={0};
+  nsubruns=0;
+  for (int hdr=0; hdr<2; hdr++) {
+    getline(in2,line);      // header
+  }
+  lastrun = 0;
+  while (true) {
+    in2 >> run >> subrun >> pin >> rms;
+    if (!in2.good()) break;
+    if (TEST && lastrun!=0 && run!=lastrun) continue; // only want one run
+    subrunpin2[subrun] = pin;
+    subrunrms2[subrun] = rms;
+    if (run == lastrun) continue; // only do this once per run
+    lastrun = run;
+    //if (VERBOSE) printf("%6d %2d %5d %.2f\n", run, subrun, pin, rms);
+    int subrunevs2[40], subrunhits2[40];
+    get_subrun_nhit(run, subrunevs2, subrunhits2, (bool)IS_MC, (bool)TEST);
+    for (int sr=0; sr<40; sr++) {
+      if(subrunevs2[sr]==0) continue;
+      avgnhit2[sr] = (float)subrunhits2[sr]/subrunevs2[sr];
+      //if (VERBOSE) printf("Run %6d subrun %2d has %4d events with an average nhit of %5.2f.\n", run, sr, subrunevs[sr], avgnhit[sr]);
+      nsubruns++;
+    }
+  }
+  printf("Ran over %d subruns.\n",nsubruns);
+  if (nsubruns==0) {
+    cerr<<"*** ERROR *** No input files found."<<endl;
+    return 1; 
+  }
+  TGraph *g2 = new TGraph(nsubruns,subrunpin2,avgnhit2);
+  
 
   // Plot graph
-  TGraph *g = new TGraph(20,subrunpin,avgnhit);
   TCanvas *c = new TCanvas("c","",800,600);
   c->SetGrid();
-  g->SetTitle("TELLIE run 100801;PIN reading;Average NHit");
+  string title = Form("TELLIE fibre FT001A;PIN reading;Average NHit",TEST);
+  c->DrawFrame(680,25,740,45)->SetTitle(title.c_str());
+  //g->SetTitle(title.c_str());
   g->SetMarkerStyle(7);
   g->SetMarkerColor(2);
-  g->Draw("AP");
+  g->Draw("P");
+  g2->SetMarkerStyle(7);
+  g2->SetMarkerColor(4);
+  g2->Draw("P same");
+  c->Update();
   c->Print("pin_readings.pdf");
   c->Print("pin_readings.png");
   c->Close();
 
   // End of program
   if (g) delete g;
+  if (g2) delete g2;
   if (c) delete c;
   return 0;
 }
@@ -156,27 +199,6 @@ void get_subrun_nhit(int run, int *srevts, int *srhits, bool isMC=false, bool TE
   const RAT::DU::PMTInfo& pmtinfo = RAT::DU::Utility::Get()->GetPMTInfo();
   const int NPMTS = pmtinfo.GetCount();
 
-  /*
-  // Get fibre information (without using RATDB) 
-  string fibre_table = "Fibre_Positions_DocDB1730.csv";
-  ifstream tab(fibre_table.c_str());
-  if (!tab) { cerr<<"Failed to open "<<fibre_table<<endl; exit(1); }
-  string line, ff, fn;
-  double fx,fy,fz,fu,fv,fw;
-  getline(tab,line);      // header
-  while (tab.good()) {
-    tab >> ff >> fx >> fy >> fz >> fu >> fv >> fw >> fn;
-    if (!tab.good()) break;
-    if (ff==fibre) break;
-  }
-  if(ff!=fibre) { cerr << "Failed to find information for fibre " << fibre << endl; exit(1); }
-  //cout << Form("%s %f %f %f %f %f %f %s\n",ff.c_str(),fx,fy,fz,fu,fv,fw,fn.c_str()) << endl;
-  TVector3 fibrepos(10*fx,10*fy,10*fz); // position of fibre [mm]
-  TVector3 fibredir(fu,fv,fw); // direction of fibre
-  TVector3 lightpos = fibrepos + 2*fibrepos.Mag()*fibredir; // projected light spot centre 
-  //cout << "DocDB: fibre " << fibre << ", pos " << printVector(fibrepos) << ", dir " << printVector(fibredir) << endl;
-  */
-
   // ********************************************************************
   // Sum PMT hit counts for entire run
   // ********************************************************************
@@ -208,51 +230,4 @@ void get_subrun_nhit(int run, int *srevts, int *srhits, bool isMC=false, bool TE
     }
     return;
 
-  /*
-    // Write PMT hit count to output file (saves time when rerunning)
-    FILE *outFile = fopen(out.c_str(),"w");
-    TVector3 pmtpos;
-    fprintf(outFile, "Run: %d\n", run);             // run number
-    fprintf(outFile, "Events: %d\n", count);        // events with EXTA trigger
-    fprintf(outFile, "TotalNHit: %d\n", totalnhit); // calibrated PMT hits with removed crosstalk
-    for(int id=0; id<NPMTS; id++) {
-      pmtpos = pmtinfo.GetPosition(id);
-      int in_spot = 0;
-      if (pmtpos.Angle(lightpos) < DIR_CONE/180*pi) in_spot = 1;
-      else if (pmtpos.Angle(fibrepos) < REF_CONE/180*pi) in_spot = 2;
-      fprintf(outFile, "%d %d %d\n", id, pmthitcount[id], in_spot);
-    }
-    fclose(outFile);
-    scanned_file = 1;
-  */
-   
-  // ********************************************************************
-  // Plotting section
-  // ********************************************************************
-  /*
-  TCanvas *c0 = new TCanvas("","",1200,1500);
-  gStyle->SetTitleOffset(1.2,"xyz");
-  TPad *pad0 = new TPad("pad0","Text",0.01,0.75,0.50,0.99);
-  TPad *pad1 = new TPad("pad1","Hist",0.50,0.75,1.00,0.99);
-  TPad *pad2 = new TPad("pad2","Icos",0.05,0.41,0.95,0.74);
-  TPad *pad3 = new TPad("pad3","Hist",0.01,0.01,0.50,0.40);
-  TPad *pad4 = new TPad("pad4","Hist",0.50,0.01,1.00,0.40);
-  pad0->Draw();
-  pad1->Draw();
-  pad2->Draw();
-  pad3->Draw();
-  pad4->Draw();
-  
-  // Save canvas and close
-  string outfile;
-  if (!TEST) outfile = Form("images/PCA_%s",fibre.c_str());
-  else       outfile = "pin_readings";
-  c0->Print(Form("%s.png",outfile.c_str()));
-  c0->Print(Form("%s.pdf",outfile.c_str()));
-  c0->Close();
-  */ 
-  // Delete pointers created with 'new'
-  //if(hhits) delete hhits;
-  //if(c0) delete c0;
-  
 }
