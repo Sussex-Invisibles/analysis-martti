9a526348a60431314181191dbc0109c31b24fe68
mnirkko <m.nirkko@sussex.ac.uk>
2018-03-02 02:28:03 +0000

    Improvements.

--- ambe_mc/template/resubmit_failed_jobs.sh
+++ ambe_mc/template/resubmit_failed_jobs.sh
@@ -35,8 +35,10 @@ count=0
 for bashlog in ${DIR}/logs/bash/*; do
 
   # Skip jobs that completed successfully
-  last=`tail $b -n 1`
-  if [[ $last == "Finished job script"* ]]; then continue; fi
+  last=`tail ${bashlog} -n 1`
+  if [[ ${last} == "Finished job script"* ]]; then
+    continue
+  fi
   
   # Reset/increase counters
   (( count++ ))
@@ -44,22 +46,38 @@ for bashlog in ${DIR}/logs/bash/*; do
   
   # Find bash log of failed job
   index=`echo ${bashlog} | sed 's/.*\.//'`
-  if [ -f ${bashlog} ] ; then (( found++ )) ; fi
+  if [ -f ${bashlog} ] ; then 
+    (( found++ ))
+  else
+    echo "WARNING: Could not find BASH log: ${bashlog}"
+  fi
   
   # Find RAT log of failed job
   line=`grep "Hostname: node" ${bashlog}`
   node=`echo $line | cut -d " " -f 2 | sed 's/[^0-9]*//g'`
   pid=`echo $line | cut -d " " -f 4`
   ratlog=${DIR}/logs/rat_logs/rat.node${node}.${pid}.log
-  if [ -f ${ratlog} ] ; then (( found++ )) ; fi
+  if [ -f ${ratlog} ] ; then
+    (( found++ ))
+  else
+    echo "WARNING: Could not find RAT log: ${ratlog}"
+  fi
   
   # Find ROOT output file of failed job
   rootfile=${DIR}/output/AmBe_${1}_${index}.root
-  if [ -f ${rootfile} ] ; then (( found++ )) ; fi
+  if [ -f ${rootfile} ] ; then
+    (( found++ ))
+  else
+    echo "WARNING: Could not find ROOT file: ${rootfile}"
+  fi
   
   # Find macro that submitted failed job
   macro=${DIR}/macros/AmBe_${1}_${index}.mac
-  if [ -f ${macro} ] ; then (( found++ )) ; fi
+  if [ -f ${macro} ] ; then
+    (( found++ ))
+  else
+    echo "WARNING: Could not find RAT macro: ${macro}"
+  fi
   
   # Delete files ONLY if all 4 files were found
   if [ $found -ne 4 ]; then
