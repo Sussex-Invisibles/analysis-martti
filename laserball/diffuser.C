8109b9b678bb77ee1a215b8ee7408d21424e8203
mnirkko <m.nirkko@sussex.ac.uk>
2018-09-25 19:20:09 +0100

    Increase verbosity options and update specs from 3M company.

--- laserball/diffuser.C
+++ laserball/diffuser.C
@@ -8,7 +8,7 @@
 #include "../HelperFunc.C"
 
 // Global constants
-const int VERBOSE = 1; // verbosity (1=lots)
+const int VERBOSE = 1; // verbosity (0-3)
 const int NEVENTS = 1e6; // number of photons
 const int NBINS = 100; // number of bins
 
@@ -20,14 +20,14 @@ const double lambda = 4.05e-5; // wavelength [cm]
 // Diffuser ball - TODO REVIEW ALL PARAMETERS
 const double flaskdiam = 106.; // diameter of the diffuser flask [mm]
 const double R = flaskdiam/2.; // radius
-const double na = 1.000; // refractive index (air)
+const double na = 1.200; // refractive index (air) - TODO is actually 2:1 mixture of SO2:O2 at 1/3 atm!
 const double ns = 1.404; // refractive index (SilGel)
 const double ng = 1.530; // refractive index (glass)
 
 // Glass bubbles
-const double r_bub = 2.e-3; // average radius of hollow glass microsphere [cm]
-const double d_bub = 1.e-4; // approx. wall thickness of hollow sphere [cm] - TODO get reference
-const double rho_bub = 2.23; // density of borosilicate glass [g/cm³] - TODO get reference
+const double r_bub = 2.e-3; // median radius of hollow glass microsphere [cm] - 50% percentile
+const double d_bub = 8.8e-5; // approx. wall thickness of hollow sphere [cm] - specs from 3M
+const double rho_bub = 2.25; // density of borosilicate glass [g/cm³] - specs from 3M
 const double con_bub = 4.84e-3; // density of bubbles in silgel [g/cm³] - TODO use optimum value ~1.57 mg/mL
 
 // Quartz rod
@@ -42,7 +42,7 @@ const double cs = c0/ns; // speed of light in SilGel [mm/ns]
 
 // *****************************************************************************
 // Function declarations
-TVector3 diffuser(double& tracklen);
+TVector3 diffuser(double& tracklen, double scatlen);
 double distance_to_wall(TVector3&, TVector3&);
 double reflection_prob(const double& n1, const double& n2, const double &impact);
 TVector3 propagate(TVector3&, TVector3&, double& distance);
@@ -65,6 +65,9 @@ int main(int argc, char** argv) {
 
   // Set output verbosity (kPrint, kInfo, kWarning, kError, kBreak, kSysError, kFatal)
   gErrorIgnoreLevel = kWarning;
+
+  // Scattering length of photons in diffuser medium
+  double scatlen = 10*GetScatteringLength(con_bub); // mean free path [mm]
   
   // Initialisation
   gen->SetSeed(0);
@@ -91,7 +94,7 @@ int main(int argc, char** argv) {
     // Generate events
     for (event=0; event<NEVENTS; event++) {
       printProgress(event,NEVENTS);
-      outdir = diffuser(length);
+      outdir = diffuser(length, scatlen);
       hesc.Fill(length/cs); // assume constant speed in silicone, neglect time spend in bubbles
       hphi.Fill(outdir.Phi()/pi);
       hcth.Fill(cos(outdir.Theta()));
@@ -142,10 +145,7 @@ int main(int argc, char** argv) {
 // Track a single photon through the diffuser flask
 // Inputs: none
 // Output: escape direction of photon, seen from 6m sphere
-TVector3 diffuser(double& tracklen) {
-
-  // Scattering length of photons in diffuser medium
-  double scatlen = 10*GetScatteringLength(con_bub); // mean free path [mm]
+TVector3 diffuser(double& tracklen, double scatlen) {
   
   // Photon position at injection point
   pos = e1;
@@ -177,10 +177,10 @@ TVector3 diffuser(double& tracklen) {
     trackT.SetPoint(step,pos.X(),pos.Y());
     trackU.SetPoint(step,pos.Y(),pos.Z());
   }
-  if (VERBOSE) printf("ENTERING diffuser at (%8.3f %8.3f %8.3f), R=%6.3f\n",pos.X(),pos.Y(),pos.Z(),pos.Mag());
+  if (VERBOSE > 1) printf("ENTERING diffuser at (%8.3f %8.3f %8.3f), R=%6.3f\n",pos.X(),pos.Y(),pos.Z(),pos.Mag());
   bool exitflask = false;
   while (!exitflask) {
-    if (VERBOSE) printf("position (%8.3f %8.3f %8.3f), direction = (%8.3f %8.3f %8.3f)\n",pos.X(),pos.Y(),pos.Z(),dir.X(),dir.Y(),dir.Z());
+    if (VERBOSE > 2) printf("position (%8.3f %8.3f %8.3f), direction = (%8.3f %8.3f %8.3f)\n",pos.X(),pos.Y(),pos.Z(),dir.X(),dir.Y(),dir.Z());
     double y = gen->Rndm();
     double dsel = -scatlen*log(y);  // randomly selected distance in diffuser [mm]
     double dwall = distance_to_wall(pos,dir); // distance to flask wall [mm]
@@ -212,10 +212,10 @@ TVector3 diffuser(double& tracklen) {
     }
   }
   
-  if (VERBOSE) printf("EXITING diffuser after %d scatters at (%8.3f %8.3f %8.3f), R=%6.3f\n",step,pos.X(),pos.Y(),pos.Z(),pos.Mag());
+  if (VERBOSE > 1) printf("EXITING diffuser after %d scatters at (%8.3f %8.3f %8.3f), R=%6.3f\n",step,pos.X(),pos.Y(),pos.Z(),pos.Mag());
   double RMAX = 6e3; // AV radius [mm]
   endpos = propagate(pos,dir,RMAX);
-  if (VERBOSE) printf("End point (%8.3f %8.3f %8.3f), R=%6.3f\n",endpos.X(),endpos.Y(),endpos.Z(),endpos.Mag());
+  if (VERBOSE > 1) printf("End point (%8.3f %8.3f %8.3f), R=%6.3f\n",endpos.X(),endpos.Y(),endpos.Z(),endpos.Mag());
   if (!event) {
     trackS.SetPoint(step+1,endpos.X(),endpos.Z());
     trackT.SetPoint(step+1,endpos.X(),endpos.Y());
@@ -368,7 +368,7 @@ TVector3 reflect_or_refract(TVector3& pos, TVector3& dir, double& impact, bool& 
     if (roll < prob) {
       scatdir = dir-2.*prod*pos;
       exitflask = false;
-      if (VERBOSE) printf("REFLECTING on diffuser wall at (%8.3f %8.3f %8.3f), R=%6.3f\n",x,y,z,pos.Mag());
+      if (VERBOSE > 1) printf("REFLECTING on diffuser wall at (%8.3f %8.3f %8.3f), R=%6.3f\n",x,y,z,pos.Mag());
     }
     // (C) refract out of the diffuser
     else {
@@ -416,7 +416,7 @@ TVector3 reflect_or_refract(TVector3& pos, TVector3& dir, double& impact, bool& 
       scatdir.SetXYZ(bet,0,alp);
       
       // Check that rotated vectors are all in x-z plane
-      if (VERBOSE) {
+      if (VERBOSE > 2) {
         printf("-----\n");
         printf("rotpos = (%8.3f %8.3f %8.3f), length=%8.3f\n",rotpos.X(),rotpos.Y(),rotpos.Z(),rotpos.Mag());
         printf("rotdir = (%8.3f %8.3f %8.3f), length=%8.3f\n",rotdir.X(),rotdir.Y(),rotdir.Z(),rotdir.Mag());
@@ -433,7 +433,7 @@ TVector3 reflect_or_refract(TVector3& pos, TVector3& dir, double& impact, bool& 
       scatdir.RotateUz(pos.Unit());
       
       // Check output vectors
-      if (VERBOSE) {
+      if (VERBOSE > 2) {
         printf("-----\n");
         printf("rotpos = (%8.3f %8.3f %8.3f), length=%8.3f\n",rotpos.X(),rotpos.Y(),rotpos.Z(),rotpos.Mag());
         printf("rotdir = (%8.3f %8.3f %8.3f), length=%8.3f\n",rotdir.X(),rotdir.Y(),rotdir.Z(),rotdir.Mag());
@@ -442,7 +442,7 @@ TVector3 reflect_or_refract(TVector3& pos, TVector3& dir, double& impact, bool& 
       }
       
       // Verify output vectors are correct
-      if (VERBOSE) {
+      if (VERBOSE > 2) {
         printf("CHECK RESULTS\n");
         double d = sqrt(1.-pow(ns/ng,2)*(1.-pow(fabs(prod),2)));
         printf("(1) Snell's law: 0 = %e\n",scatdir*pos.Unit()-d);
@@ -462,25 +462,27 @@ TVector3 reflect_or_refract(TVector3& pos, TVector3& dir, double& impact, bool& 
 // *****************************************************************************
 // Get scattering length from theory, given a bubble mass density
 double GetScatteringLength(double density) {
+  if (VERBOSE > 0) cout << "Provided mass density of glass bubbles: " << density << " g/cm^3" << endl;
+  
   // Scattering cross section of glass microspheres
   // https://en.wikipedia.org/wiki/Anomalous_diffraction_theory
   // https://www.gfdl.noaa.gov/wp-content/uploads/files/user_files/pag/lecture2008/lecture3.pdf
   double n = ns/na; // refractive index (outer vs inner)
-  double p = 4.*pi*r*(n-1.)/lambda; // phase delay of wave through sphere
+  double p = 4.*pi*r_bub*(n-1.)/lambda; // phase delay of wave through sphere
   double Q = 2. - 4./p*sin(p) + 4./(p*p)*(1.-cos(p)); // scattering efficiency (same as extinction efficiency)
   double sigma = pi*r_bub*r_bub*Q; // scattering cross section [cm²]
-  if (VERBOSE) cout << "Estimated scattering cross section: " << sigma << " cm^2" << endl;
+  if (VERBOSE > 0) cout << "Estimated scattering cross section: " << sigma << " cm^2" << endl;
   
   // Number density of glass microspheres (very approximate)
   double V = 4.*pi/3.*(pow(r_bub,3) - pow(r_bub-d_bub,3)); // volume of glass [cm³]
   double m = rho_bub*V; // mass of hollow sphere [g]
   double N = density/m; // number concentration [cm⁻³]
-  if (VERBOSE) cout << "Estimated number concentration of bubbles: " << N << "/cm^3" << endl;
+  if (VERBOSE > 0) cout << "Estimated number density of bubbles: " << N << "/cm^3" << endl;
   
   // Scattering parameters
   double eps = N*sigma; // scattering coefficient [cm⁻¹]
   double L = 1./eps; // scattering length [cm]
-  if (VERBOSE) cout << "Estimated scattering length: " << L << " cm" << endl;
+  if (VERBOSE > 0) cout << "Estimated scattering length: " << L << " cm" << endl;
 
   return L;
 }
