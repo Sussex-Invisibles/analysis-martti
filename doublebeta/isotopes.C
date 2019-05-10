54802e6ec30d3f0ddfe70de51b611aab0c93199c
mnirkko <m.nirkko@sussex.ac.uk>
2019-05-10 17:13:32 +0100

    Added many more isotopes, improved plot.

--- doublebeta/isotopes.C
+++ doublebeta/isotopes.C
@@ -10,36 +10,52 @@
 
 int main() {
 
-  // Number of DBD isotopes
-  const int N = 16;
+  // Max. number of isotopes
+  const int N = 100;
   
   // Read from file
   int a[N];
   string s[N], t[N];
   float x[N], y[N];
-  int A, Q, it=0;
+  int A, Q, count=0;
   float IA, G, H;
   string S, line;
-  ifstream in("isotopes.dat");
+  ifstream in("isotopes2.dat");
   getline(in,line);
-  while (in >> A >> S >> Q >> IA >> G >> H) {
-    a[it] = A;
-    s[it] = S;
-    x[it] = IA; // %
-    y[it] = Q/1e3; // MeV
+  while (in >> A >> S >> Q >> IA) {
+    if (IA==0) continue; // isotope doesn't exist naturally
+    a[count] = A;
+    s[count] = S;
+    x[count] = IA; // %
+    y[count] = Q/1e3; // MeV
+    count++;
+  }
+  cout << "Found " << count << " isotopes." << endl;
+  
+  // Generate graph (all isotopes)
+  TGraph g(count,x,y);
+  g.SetMarkerStyle(4);
+  g.SetMarkerColor(2);
+  
+  // Graph with only relevant isotopes
+  TGraph h(9);
+  int it=0;
+  for (int i=0; i<count; i++) {
+    if (x[i]==0) continue; // isotope doesn't exist naturally
+    if (a[i]!=48 && x[i]<2.0) continue; // low abundance (except 48Ca)
+    if (a[i]!=76 && y[i]<2.4) continue; // low Q-value (except 76Ge)
+    cout << a[i] << s[i] << endl;
+    h.SetPoint(it,x[i],y[i]);
     it++;
   }
-  
-  // Generate graph
-  TGraph g(N,x,y);
-  g.SetMarkerStyle(8);
-  g.SetMarkerColor(2);
+  h.SetMarkerStyle(8);
+  h.SetMarkerColor(2);
   
   // Use T2K style
   CommonStyle();
   gStyle->SetPadBottomMargin(0.14);
-  gStyle->SetPadLeftMargin(0.1);//to include both large/small font options
-  gStyle->SetPadRightMargin(0.03);//to include both large/small font options
+  gStyle->SetPadLeftMargin(0.1);
+  gStyle->SetPadRightMargin(0.03);
   gStyle->SetLabelSize(0.05,"xyz");
   gStyle->SetTitleSize(0.05,"xyz");
   gStyle->SetLabelOffset(0.01,"xyz");
@@ -53,33 +69,62 @@ int main() {
   TCanvas c("c","",800,600);
   c.SetGrid();
   //c.SetLogx();
-  TH1F *p = c.DrawFrame(0,0,40,5,";Natural abundance (%); Q-value (MeV)");
-  //p->GetXaxis()->SetNdivisions(-1004);
+  
+  // Plotting range
+  int XMIN = 0;
+  int XMAX = 50;
+  int YMIN = 0;
+  int YMAX = 5;
+  
+  // Draw frame
+  TH1F *p = c.DrawFrame(XMIN,YMIN,XMAX,YMAX,";Natural abundance (%); Q-value (MeV)");
+  p->GetXaxis()->SetNdivisions(-505);
   p->GetYaxis()->SetNdivisions(-505);
+  
+  // Draw markers
   g.Draw("p");
-  for (int i=0; i<N; i++) {
+  h.Draw("p");
+  
+  // Draw background lines
+  TLine b(0,0,0,0);
+  b.SetLineStyle(7);
+  b.SetLineColor(4);
+  b.DrawLine(XMIN,2.615,XMAX,2.615);
+  b.DrawLine(XMIN,3.27,XMAX,3.27);
+  
+  // Draw background text
+  l.DrawLatex(35,2.715,"#color[4]{E_{#gamma}(^{208}Tl) = 2.615 MeV}");
+  l.DrawLatex(35,3.37,"#color[4]{E_{#beta}(^{214}Bi) = 3.27 MeV}");
+  
+  // Draw isotope names near markers
+  for (int i=0; i<count; i++) {
+    if (x[i]==0) continue; // isotope doesn't exist naturally
+    if (a[i]!=48 && x[i]<2.0) continue; // low abundance (except 48Ca)
+    if (a[i]!=76 && y[i]<2.4) continue; // low Q-value (except 76Ge)
     //cout << a[i] << s[i] << endl;
     string tmp = Form("^{%d}%s",a[i],s[i].c_str());
     switch(a[i]) {
       case 48 :
-        l.DrawLatex(x[i]+0.5,y[i]-0.1,tmp.c_str());
+        l.DrawLatex(x[i]+0.6,y[i]-0.1,tmp.c_str());
         break;
       case 76:
-      case 96 :
         l.DrawLatex(x[i]-0.2,y[i],tmp.c_str());
+        break;
+      case 82 :
+        l.DrawLatex(x[i]-3.0,y[i],tmp.c_str());
+        break;
+      case 96 :
+        l.DrawLatex(x[i]-1.0,y[i]+0.1,tmp.c_str());
         break;
       case 100 :
         l.DrawLatex(x[i]-0.5,y[i],tmp.c_str());
         break;
-      case 82 :
-        l.DrawLatex(x[i]-2.5,y[i],tmp.c_str());
-        break;
       case 116 :
-      case 124 :
-        l.DrawLatex(x[i]-3.1,y[i]-0.05,tmp.c_str());
+      case 136 :
+        l.DrawLatex(x[i]-3.8,y[i]-0.1,tmp.c_str());
         break;
-      case 148 :
-        l.DrawLatex(x[i]-1.8,y[i]-0.25,tmp.c_str());
+      case 130 :
+        l.DrawLatex(x[i]-2.0,y[i]-0.25,tmp.c_str());
         break;
       default :
         l.DrawLatex(x[i]-0.5,y[i],tmp.c_str());
